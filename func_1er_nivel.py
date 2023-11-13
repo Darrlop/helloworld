9f707bd8a4af45e84fb117933478638a12b9d105
DAL <149305694+Darrlop@users.noreply.github.com>
2023-11-13 13:05:24 +0100

    " "

--- func_1er_nivel.py
+++ func_1er_nivel.py
@@ -1,3 +1,5 @@
+import func_1er_nivel_return as f1r
+
 def normal(x):
     return x
 
@@ -12,5 +14,14 @@ def suma_todos(limite, f):
         resultado += f(i)
     return resultado
 
+
+
+
+
 print(suma_todos(10, normal))
 print(suma_todos(10, cuadrado))
+print(f1r.maximo([1,2,3,10,5,6]))
+print(f1r.media([0,1,2,3,4]))
+z = f1r.return_funcion("med")
+z([0,1,2,3,4]) 
+print("Pasando al print directamente la devo de restur_funcion: {}".format(f1r.return_funcion("med")([0,1,2,3,4])))
